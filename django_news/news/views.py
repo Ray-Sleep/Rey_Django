d8ca4718a64be409bd3ef9b7d2ea3cbfcf7f5007
Sleep-Ray <lishurui520@qq.com>
2023-09-15 04:15:19 +0800

    完善模板和视图

--- django_news/news/views.py
+++ django_news/news/views.py
@@ -17,5 +17,18 @@ def user_list(request):
     # 注意，这里使用的是相对路径，相对于 app 目录.
     return render(request, 'user_list.html')
 
+
 def user_add(request):
-    return render(request, 'user_add.html')
+    # 字符串
+    name = 'Rey'
+    # 列表
+    roles = ['admin', 'ceo']
+    # 字典
+    user_info = {"name":'Rey',"salary":10000,"role":'ceo'}
+    # 列表套字典
+    data_list = [
+        {"name":'Rey',"salary":10000,"role":'ceo'},
+        {"name":'Tom',"salary":8000,"role":'admin'},
+        {"name":'Jerry',"salary":3000,"role":'保安'}
+    ]
+    return render(request, 'user_add.html', {"n1": name,"n2":roles,"n3":user_info,"n4":data_list})
